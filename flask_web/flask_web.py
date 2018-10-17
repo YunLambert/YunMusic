425eb6d9dd0d01e7bdf4cc4607a49b6b15955494
YunLambert <469066072@qq.com>
2018-10-17 15:31:31 +0800

    add 跳转逻辑

--- flask_web/flask_web.py
+++ flask_web/flask_web.py
@@ -1,33 +1,45 @@
-from flask import Flask,request,render_template,redirect,url_for
-from flask.ext.wtf import Form
-from wtforms import StringField,SubmitField
+from flask import Flask, request, render_template, redirect, url_for
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "12345678"
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method=='POST':
-        return redirect('search')
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        name=request.args.get('name')
+        NameSaved.name=name
+        return redirect(url_for('search',name=name))
     return render_template('index.html')
 
-@app.route('/search')
+
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-    return render_template('search_item.html')
+    name=NameSaved.name
+    return render_template('search_item.html',name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'),500
+    return render_template('500.html'), 500
 
 
 class NameForm(Form):
-    name=StringField("Search song")
-    submit=SubmitField("Submit")
+    name = StringField("Search song")
+    submit = SubmitField("Submit")
+
+class NameSaved():
+    name=''
+
 
 if __name__ == '__main__':
     app.run(debug=True)
