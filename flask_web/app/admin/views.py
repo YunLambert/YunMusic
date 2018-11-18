edf20197c423da18230801c02ee12365218db7dd
YunLambert <469066072@qq.com>
2018-11-18 22:22:19 +0800

    fix some bugs

--- flask_web/app/admin/views.py
+++ flask_web/app/admin/views.py
@@ -5,7 +5,7 @@ from flask import render_template,redirect,url_for
 
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'> this is admin page!</h1>"
+    return render_template("admin/index.html")
 
 @admin.route("/login/")
 def login():
@@ -14,4 +14,13 @@ def login():
 @admin.route("/logout/")
 def logout():
     return redirect(url_for("admin.login"))
+
+@admin.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@admin.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
--- flask_web/manage.py
+++ flask_web/manage.py
@@ -6,45 +6,5 @@ from wtforms import StringField, SubmitField
 # app = Flask(__name__)
 # app.config["SECRET_KEY"] = "12345678"
 
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'GET':
-#         return render_template('index.html')
-#     if request.method == 'POST':
-#         name=request.form.get('name')
-#         NameSaved.name=name
-#         return redirect(url_for('search',name=name))
-#     return render_template('index.html')
-#
-#
-# @app.route('/search', methods=['GET'])
-# def search():
-#     name=NameSaved.name
-#     print(name)
-#     return render_template('search_item.html',name=name)
-#     #return u'the search is:%s' % name
-#
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return render_template('404.html'), 404
-#
-#
-# @app.errorhandler(500)
-# def internal_server_error(e):
-#     return render_template('500.html'), 500
-#
-# @app.route('/contacts', methods=['GET'])
-# def contacts():
-#     return render_template('contacts.html')
-#
-# class NameForm(Form):
-#     name = StringField("Search song")
-#     submit = SubmitField("Submit")
-#
-# class NameSaved():
-#     name='hell0'
-
-
 if __name__ == '__main__':
     app.run(debug=True)
